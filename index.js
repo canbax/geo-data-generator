74544494b45b4f99d073727d7d735f60623ebdbd
canbax <yusufsaidcanbaz@gmail.com>
2022-12-10 10:11:13 +0300

    fix turkish city names for turkish characters

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ const t1 = new Date().getTime();
 
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
+const eng2tr = {
+  Istanbul: "İstanbul",
+  Izmir: "İzmir",
+  Kahramanmaras: "Kahramanmaraş",
+  Kutahya: "Kütahya",
+  Canakkale: "Çanakkale",
+  Nigde: "Niğde",
+  Diyarbakir: "Diyarbakır",
+  Sanliurfa: "Şanlıurfa",
+  Sirnak: "Şırnak",
+  Aydin: "Aydın",
+  Mugla: "Muğla",
+  Elazig: "Elazığ",
+  Nevsehir: "Nevşehir",
+  Duzce: "Düzce",
+  Corum: "Çorum",
+  Gumushane: "Gümüşhane",
+  Eskisehir: "Eskişehir",
+  Tekirdag: "Tekirdağ",
+  Agri: "Ağrı",
+  Kirikkale: "Kırıkkale",
+  Adiyaman: "Adıyaman",
+  Usak: "Uşak",
+  Balikesir: "Balıkesir",
+  Bingol: "Bingöl",
+  Karabuk: "Karabük",
+  Cankiri: "Çankırı",
+  Bartin: "Bartın",
+  Kirsehir: "Kırşehir",
+  Kirklareli: "Kırklareli",
+  Mus: "Muş",
+  Igdir: "Iğdır",
+};
+
 bar1.start(4969097, 0);
 
 fs.createReadStream("./IP2LOCATION-LITE-DB5.IPV6.CSV")
@@ -17,12 +51,18 @@ fs.createReadStream("./IP2LOCATION-LITE-DB5.IPV6.CSV")
     bar1.increment(1);
     const countryCode = row[2];
     const countryName = row[3];
-    const regionName = row[4];
-    const cityName = row[5];
+    let regionName = row[4];
+    let cityName = row[5];
     const lat = row[6];
     const lng = row[7];
     if (countryCode === "-" || countryName === "-" || regionName === "-") {
       return;
+    }
+    if (eng2tr[regionName]) {
+      regionName = eng2tr[regionName];
+    }
+    if (eng2tr[cityName]) {
+      cityName = eng2tr[cityName];
     }
     if (!data[countryName]) {
       data[countryName] = { code: countryCode, regions: {} };
